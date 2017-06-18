4107a9a2d5686a5f231c9ac30390f69954749019
Harold Cris <programmer582@hotmail.com>
2017-06-19 02:19:01 +0800

    Initial Commit

---
+++ Aitech.Utility/Database.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Aitech.Utility
+{
+    public class Database
+    {
+        public static SqlConnection CreateConnection()
+        {
+            var builder = new SqlConnectionStringBuilder()
+            {
+                DataSource = ".",
+                InitialCatalog = "api",
+                IntegratedSecurity = true
+            };
+
+            return new SqlConnection(builder.ToString());
+        }
+    }
+}
